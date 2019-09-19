3a5cda3c155e75a42ed4bb92850c1461f2b26079
Qawdaya <Qawdaya@LAPTOP-F1P37JFE>
2019-09-19 14:08:01 +0800

    a

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,10 +6,27 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
-         
-        }
+            int i;
+            for(i=0;i<=100;i++)
+            {  
+                Console.Write( " "+i);
+                int j;
+                j = i % 10;
+               if(j==0)
+                {
+                    Console.WriteLine();
+                }
+            }
+         /*                                  */
+            for(int a=0;a<100;a+=10)
+            {
+                for(int b=a;b<a+10;b++)
+                {
+                    Console.Write(" " + b);
+
+                }
+                Console.WriteLine();
+            }
+         }
     }
 }
