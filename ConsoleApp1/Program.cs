8fcfaeaf74f9e720dcf836513a5d731e21554e17
Qawdaya <Qawdaya@LAPTOP-F1P37JFE>
2019-09-19 15:22:11 +0800

    string 类型

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,17 +4,19 @@ namespace ConsoleApp1
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            /*0~10打印*/
-            for (int i = 0; i <= 10; i++)
-            {
-                Console.WriteLine(i);
-            }
-            for (int i = 10; i > 0; i--)
-            { Console.Write(i); }
-            // Console.ReadLine();
+            string s1 = "a string";
+            string s2 = s1;
+            Console.WriteLine("s1 is" + s1);
+            Console.WriteLine("s2 is" + s2);
+            s1 = "another string";
+            Console.WriteLine("s1 now is" + s1);
+            Console.WriteLine("s2 now is" + s2);
+            Console.ReadLine();
+            
         }
     }
-    }
+    
 }
