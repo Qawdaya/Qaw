64f682479c1e6c6f86f5f1b3227a86477f9578cf
Qawdaya <Qawdaya@LAPTOP-F1P37JFE>
2019-09-19 17:10:37 +0800

    switch语句

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,17 +7,51 @@ namespace ConsoleApp1
         
         static void Main(string[] args)
         {
-            int i=8;
-          
-            bool isZero;
-            if(i==0)
-            { isZero = true;
-                Console.WriteLine("i is zero.");
-            }
-            else
+           // Random rd = new Random();
+            int i;
+            // i = rd.Next();
+            i = 2;
+            switch (i)
             {
-                isZero = false;
-                Console.WriteLine("i is  not zero.");
+                case 1:
+                    Console.WriteLine("i=1");
+                    break;
+                case 2:
+                    Console.WriteLine("i=2");
+                    break;
+                case 3:
+                    Console.WriteLine("i=3");
+                    break;
+                case 4:
+                    Console.WriteLine("i=4");
+                    break;
+                default:
+                    Console.WriteLine("i is not 1,2,3 or 4");
+                    break;
+            }
+            string country = "us";
+            switch (country)
+            {
+                case "America":
+                    goto case "Britain";
+                   
+                case "Britain":
+                   Console.WriteLine(" language = English");
+                    break;
+
+
+            }
+            switch (country)
+            {
+                case "au":
+                case "uk":
+                case "us":
+                    Console.WriteLine("language=English");
+                    break;
+                case "at":
+                case "de":
+                    Console.WriteLine("language=Geman");
+                    break;
             }
         }
     }
