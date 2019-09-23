5d89545dd89eef33cfe5527aafaa7301f5235304
Qawdaya <Qawdaya@LAPTOP-F1P37JFE>
2019-09-23 15:58:42 +0800

    多个main()方法

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,44 +2,22 @@
 
 namespace ConsoleApp1
 {
-    public enum TimeofDay
-    {
-        morning=0,
-            afternoon=1,
-            evening=2
-    }
     class Program
     {
-        
-        static void Main(string[] args)
+        public static void Main()
         {
-           /*获取枚举字符串*/
-            TimeofDay time = TimeofDay.morning;
-            Console.WriteLine(time.ToString());
-            /*枚举传递值*/
-            WriteGreeting(TimeofDay.morning);
-            /*字符串中获取枚举值*/
-            TimeofDay time2 = (TimeofDay)Enum.Parse(typeof(TimeofDay), "morning", true);
-            Console.WriteLine((int)time2);
+            Demo.Main();
+            Console.ReadLine();
+            
         }
-        static void WriteGreeting(TimeofDay timeofDay)
+        class Demo
         {
-            switch(timeofDay)
+            public static void Main()
             {
-                case TimeofDay.morning:
-                    Console.WriteLine("Good morning!");
-                    break;
-                case TimeofDay.afternoon:
-                    Console.WriteLine("Good afternoon!");
-                    break;
-                case TimeofDay.evening:
-                    Console.WriteLine("Good evening!");
-                    break;
-                default:
-                    Console.WriteLine("Hello!");
-                    break;
+                Console.WriteLine("我是Demo类的Main方法！");
             }
         }
+
     }
-    
 }
+
