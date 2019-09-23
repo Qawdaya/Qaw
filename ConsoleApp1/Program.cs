3af7f2c754bd619341f76e58b9d81838c6873e07
Qawdaya <Qawdaya@LAPTOP-F1P37JFE>
2019-09-23 16:41:33 +0800

    控制台I/O

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,19 +4,20 @@ namespace ConsoleApp1
 {
     class Program
     {
-        public static void Main()
+        static void Main(string[] args)
         {
-            Demo.Main();
-            Console.ReadLine();
-            
+            int i = 10;
+            int j = 20;
+            Console.WriteLine("{0} plus {1} equal {2}",i,j,i+j);
+            Console.WriteLine("{0,4} plus {1,4} equal {2,4}", i, j, i + j);
+            Console.WriteLine("\n{0,10}\n plus {1,4}\n ------------\nequal {2,4}", i, j, i + j);
+
+            double A = 0.234;
+            Console.WriteLine("{0:#.00}",A);
+
         }
-        class Demo
-        {
-            public static void Main()
-            {
-                Console.WriteLine("我是Demo类的Main方法！");
-            }
-        }
+      
+       
 
     }
 }
