9eb3f8fad03fa256456ac65212e4e52ad2525f09
Qawdaya <Qawdaya@LAPTOP-F1P37JFE>
2019-09-25 16:35:39 +0800

    函数成员（方法重载）

--- Chapter 3/Program.cs
+++ Chapter 3/Program.cs
@@ -4,15 +4,21 @@ namespace Chapter_3
 {
     class Program
     {
-       static void Demo (int nota,int a=10)
+       static void Demo (string result)
         {
-           System.Console.WriteLine(a + nota);
+            Demo(4);
+            Console.WriteLine("test");
+        }
+        static void Demo(int result)
+        {
+            int a;
+            a = result + 10;
+            Console.WriteLine(a) ;
         }
         static void Main(string[] args)
         {
 
-            Demo(3);
-            Demo(3, 4);
+            Demo("r");
          
 
            
