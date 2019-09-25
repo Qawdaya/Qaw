0e5a5cf0dc5ba9098d911c6e0a3442100a9038f6
Qawdaya <Qawdaya@LAPTOP-F1P37JFE>
2019-09-25 15:59:21 +0800

    函数成员（out参数）

--- Chapter 3/Program.cs
+++ Chapter 3/Program.cs
@@ -4,21 +4,18 @@ namespace Chapter_3
 {
     class Program
     {
-       static void SomeFunction(int[]ints,ref int i)
+       static void SomeFunction(out int i)
         {
-            ints[0] = 100;
             i = 100;
+
         }
         static void Main(string[] args)
         {
             int i = 0;
-            int[] ints = { 0, 1, 2, 4, 8 };
-            Console.WriteLine("i=" + i);
-            Console.WriteLine("ints[0]="+ints[0]);
-            Console.WriteLine("Calling SomeFuction.");
-            SomeFunction(ints, ref i);
-            Console.WriteLine("i=" + i);
-            Console.WriteLine("ints[0]=" + ints[0]);
+          
+            SomeFunction(out i);
+           
+            Console.WriteLine(i);
             
         }
     }
