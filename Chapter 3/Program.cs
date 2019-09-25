ffc082e0bc9463fe2ffe82deb28dd3d98ff9ed76
Qawdaya <Qawdaya@LAPTOP-F1P37JFE>
2019-09-25 15:53:02 +0800

    函数成员（ref参数） 方法的输入参数前面带有ref关键字，则该方法对变量做任何改变都会影响原始对象的值

--- Chapter 3/Program.cs
+++ Chapter 3/Program.cs
@@ -4,7 +4,7 @@ namespace Chapter_3
 {
     class Program
     {
-       static void SomeFunction(int[]ints,int i)
+       static void SomeFunction(int[]ints,ref int i)
         {
             ints[0] = 100;
             i = 100;
@@ -16,7 +16,7 @@ namespace Chapter_3
             Console.WriteLine("i=" + i);
             Console.WriteLine("ints[0]="+ints[0]);
             Console.WriteLine("Calling SomeFuction.");
-            SomeFunction(ints, i);
+            SomeFunction(ints, ref i);
             Console.WriteLine("i=" + i);
             Console.WriteLine("ints[0]=" + ints[0]);
             
