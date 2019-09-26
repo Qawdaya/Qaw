0f53295b856cfe603211e58ac00b0bc01fa6ec32
Qawdaya <Qawdaya@LAPTOP-F1P37JFE>
2019-09-26 15:31:48 +0800

    属性(只读)

--- Chapter 3/Program.cs
+++ Chapter 3/Program.cs
@@ -1,33 +1,65 @@
 ﻿using System;
-
-namespace Chapter_3
+namespace runoob
 {
-    class Program
+    class Student
     {
-       static void Demo (string result)
-        {
-            Demo(4);
-            Console.WriteLine("test");
-        }
-        static void Demo(int result)
-        {
-            int a;
-            a = result + 10;
-            Console.WriteLine(a) ;
-        }
-        static void Main(string[] args)
-        {
 
-            Demo("r");
-         
+        private string name = "not known";
+        private int age = 0;
 
-           
+  
+
+        // 声明类型为 string 的 Name 属性
+        public string Name  //只读
+        {
+            get
+            {
+                return name;
+            }
+         /*  set
+            {
+                name = value;
+            }*/
         }
-        
+
+        // 声明类型为 int 的 Age 属性
+        public int Age //只读
+        {
+            get
+            {
+                return age;
+            }
+           /* set
+            {
+                if(age<0)
+                {
+                    age = 0;
+                }
+                else
+                age = value;
+            }*/
+        }
+        //重写了object类的ToString()方法
+        public override string ToString()  
+        {
+            return  " Name = " + Name + ", Age = " + Age;
+        }
     }
-    
-  }
-   
+    class ExampleDemo
+    {
+        public static void Main()
+        {
+            // 创建一个新的 Student 对象
+            Student s = new Student();
 
-    
-
+            // 设置 student 的 name 和 age
+           // s.Name = "Zara";
+           // s.Age = 9;
+            Console.WriteLine("Student Info: {0}", s);
+            // 增加年龄
+           // s.Age += 1;
+            Console.WriteLine("Student Info: {0}", s);
+            Console.ReadKey();
+        }
+    }
+}
