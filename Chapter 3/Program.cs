4fb3b390fea4660dee1ba1cf3317859b3f5282fe
Qawdaya <Qawdaya@LAPTOP-F1P37JFE>
2019-09-29 15:23:08 +0800

    弱引用

--- Chapter 3/Program.cs
+++ Chapter 3/Program.cs
@@ -12,38 +12,24 @@ namespace Chapter_3
 
         }
     }
-    struct dimensions2  //结构
-    {
-        public double length2;//当赋值，则报错
-       public double width2;//当赋值，则报错
-        public  dimensions2(double length,double width)
-        {
-           length2 = length;   
-           width2 = width;
-        }
-        public double diagona1
-        {
-            get { return length2 * length2 + width2 * width2; }
-
-        }
-    }
+  
     class Program
     {
    
         static void Main(string[] args)
         {
-            dimensions2 point;
-            point.length2 = 2;
-           point.width2 = 4;
-           Console.WriteLine(point.diagona1.ToString()); //结构调用1
-            dimensions2 d = new dimensions2();
-            d.length2 = 3;
-            d.width2 = 5;
-            Console.WriteLine(d.diagona1.ToString());//结构调用2
-            dimensions1 c = new dimensions1();
-            c.length1 = 3;
-            c.width1 = 5;
-            Console.WriteLine(c.diagona1. ToString());//类的调用
+            dimensions1 d = new dimensions1();
+            WeakReference m = new WeakReference(new dimensions1());
+            dimensions1 length;
+            if(m.IsAlive)
+            {
+                length = m.Target as dimensions1;
+                Console.WriteLine("没被回收，直接引用原对象");
+            }
+            else
+            {
+                length = new dimensions1();
+            }
 
         }
     }
