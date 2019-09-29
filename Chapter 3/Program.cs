fb4aaa53f0844f87cdb85d433ef4e6eeadf431af
Qawdaya <Qawdaya@LAPTOP-F1P37JFE>
2019-09-29 14:04:24 +0800

    匿名类型

--- Chapter 3/Program.cs
+++ Chapter 3/Program.cs
@@ -2,47 +2,21 @@
 using System.Drawing;
 namespace Chapter_3
 {
-
-    class PersonSta
-    {
-        private static string name;//静态字段
-        public static string Name //静态属性
-        {
-            get
-            {
-                return name;
-            }
-        }
-        static PersonSta() //静态构造函数，仅执行一次
-        {
-            Console.WriteLine("静态构造函数被调用了");
-
-
-          name = "keep moving";
-            
-            Console.WriteLine("静态构造函数被调用了2");
-
-        }
-
-    }
-
     class Program
     {
-       static void Main(string[] args)
+        static void Main(string[] args)
         {
-            Console.WriteLine(PersonSta.Name);
-           
-            Console.WriteLine(PersonSta.Name);
+            var captain = new { firstname = "James", middlename = "T", Lastname = "Kirk" };
+            Console.WriteLine(captain);
+            var doctor = new { firstname = "tony", middlename = "", Lastname = "Kirk" };
+            Console.WriteLine(doctor);
+            doctor = captain;
+            Console.WriteLine(doctor);
+            var captain2 = new { doctor.firstname, doctor.middlename, doctor.Lastname };
+            Console.WriteLine(captain2);
         }
 
     }
-        
-    }
-        
-    
-    
-  
-   
 
-    
+}
 
