c61ac19d495046375758f26c787ddc007fd0db27
Qawdaya <Qawdaya@LAPTOP-F1P37JFE>
2019-09-30 16:40:40 +0800

    4.3  实现继承

--- Chapter 4/4.3  实现继承.cs
+++ Chapter 4/4.3  实现继承.cs
@@ -3,12 +3,42 @@
 namespace Chapter_4
 {
     class mydrivedClass
-    { }
+    {
+        public void setn(int i)
+        {
+            c = i;
+            
+        }
+      public interface setn2
+        {
+            int getnu2(int a);
+        }
+    
+        protected int c;
+      
+    }
+    
+    class my:mydrivedClass
+        {
+        public int Getnut()
+        {
+            return (c*c);
+        }
+        public int getnut2(int a)
+        {
+            return (a + 2);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            my m = new my();
+            int f;
+            m.setn(3);
+            f = m.Getnut();
+            Console.WriteLine(m.Getnut());
+            Console.WriteLine(m.getnut2(f));
         }
     }
 }
