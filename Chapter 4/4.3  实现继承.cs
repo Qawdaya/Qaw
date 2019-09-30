b61169f4c45742cf56edcaba8d7f9e88ab5e6d87
Qawdaya <Qawdaya@LAPTOP-F1P37JFE>
2019-09-30 15:11:26 +0800

    4.3  实现继承

---
+++ Chapter 4/4.3  实现继承.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Chapter_4
+{
+    class mydrivedClass
+    { }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
