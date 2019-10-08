6083876ca4a4bdef8b35c869f0b05040c846636f
Qawdaya <Qawdaya@LAPTOP-F1P37JFE>
2019-10-08 21:27:03 +0800

    4.5.2  派生的接口

--- Chapter 4/4.5  接口.cs
+++ Chapter 4/4.5  接口.cs
@@ -2,6 +2,7 @@
 using Wrox.ProCSharp;
 using Wrox.ProCSharp.VenusBank;
 using Wrox.ProCSharp.JupiterBank;
+using Wrox.ProCSharp.ItransferAccount;
 namespace Wrox.ProCSharp
 {
     class MainEntryPoint
@@ -17,6 +18,9 @@ namespace Wrox.ProCSharp
             jupiterAccount.Withdraw(600);
             jupiterAccount.Withdraw(100);
             Console.WriteLine(jupiterAccount.ToString());
+            ITransferBankAccount Account = new CurrentAccount();
+            Account.TransferTo(venusAccount, 100);
+            Console.WriteLine(Account.ToString());
         }
     }
 }
@@ -24,7 +28,12 @@ namespace Wrox.ProCSharp
 
 namespace Wrox.ProCSharp
 {
-    public interface IBankAccount
+    public interface ITransferBankAccount : IBankAccount
+    {
+        bool TransferTo(IBankAccount destination, decimal amount);
+    }
+
+public interface IBankAccount
     {
         void PayIn(decimal amount);
         bool Withdraw(decimal amount);
@@ -98,6 +107,49 @@ namespace Wrox.ProCSharp.JupiterBank
         {
             return String.Format("Jupiter Bank Saver: Balance = {0,6:C}", balance);
         }
+    }
+
+}
+
+namespace Wrox.ProCSharp.ItransferAccount
+{
+      public class CurrentAccount : ITransferBankAccount
+    {
+        private decimal balance;
+        public void PayIn(decimal amount)
+        {
+            balance += amount;
+        }
+        public bool Withdraw(decimal amount)
+        {
+            if (balance >= amount)
+            {
+                balance -= amount;
+                return true;
+            }
+            Console.WriteLine("Withdrawal attempt failed.");
+            return false;
+        }
+        public decimal Balance
+        {
+            get
+            {
+                return balance;
+            }
+        }
+        public bool TransferTo(IBankAccount destination, decimal amount)
+        {
+            bool result;
+            if ((result = Withdraw(amount)) == true)
+                destination.PayIn(amount);
+            return result;
+        }
+        public override string ToString()
+        {
+            return String.Format("Jupiter Bank Current Account: Balance = {0,6:C}",
+                                                                            balance);
+        }
+
     }
 
 }
