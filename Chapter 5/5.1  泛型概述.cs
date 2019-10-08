bc3bcf313f5743136fa937602281ae051c26a1c3
Qawdaya <Qawdaya@LAPTOP-F1P37JFE>
2019-10-08 22:44:29 +0800

    5.1.3  二进制代码的重用

--- Chapter 5/5.1  泛型概述.cs
+++ Chapter 5/5.1  泛型概述.cs
@@ -7,6 +7,8 @@ namespace Chapter_5
 { class myclass
     {
         
+      
+        
     }
     
     class Program
@@ -16,12 +18,19 @@ namespace Chapter_5
 
             var list = new List<int>();
             list.Add(44);
-            //list.Add("mystring");
-            //list.Add(new myclass());
-            foreach(int i in list)
+            var stringlist = new List<string>();
+            stringlist.Add("mystring");
+            var myclasslist=new List<myclass>();
+            myclasslist.Add(new myclass());
+            foreach (int i in list)
             {
                 Console.WriteLine(i);
             }
+            foreach (string i2 in stringlist)
+            {
+                Console.WriteLine(i2);
+            }
+            
         }
     }
 }
