450e8039763816c5cd56cf3ed90a1bae97d6d4fa
Qawdaya <Qawdaya@LAPTOP-F1P37JFE>
2019-10-08 18:59:37 +0800

    4.3.6  派生类的构造函数（在层次结构中添加无参数的构造函数）

--- Chapter 4/4.3  实现继承.cs
+++ Chapter 4/4.3  实现继承.cs
@@ -3,57 +3,28 @@
 namespace Chapter_4
 {
 
-   
-   public sealed class FinalClass
+    public abstract class genericcustmer
     {
-        public void method()
+        private string name;
+        public genericcustmer()
+            :base()      //可以省略
         {
-            Console.WriteLine("密封类");
+            name = "<no name>";
         }
 
     }
-    /*密封类不能作为基类被继承，但它可以继承别的类或接口。
-     class DrivedClass:FinalClass
+    class neverore60custmer:genericcustmer
     {
-
-    }*/
-    public  class myclassbase
-    {
-        public virtual void finalmethod()
-        {
-            Console.WriteLine("基类中的虚方法");
-        }
+        private int highcostminutesused;
     }
-   
-    class myclass:myclassbase
-    {
-        public sealed override void finalmethod()
-        {
-            base.finalmethod();
-            Console.WriteLine("密封中重写后的方法");
-        }
-    }
-    /*并不是每个方法都可以声明为密封方法，密封方法只能用于对基类的虚方法进行实现，并提供具体的实现。
-     * 所以，声明密封方法时，sealed 修饰符总是和 override 修饰符同时使用。
-     
-     * class drivedclass:myclass
-    {
-        public override void finalmethod()
-        {
-
-        }
-    }*/
 
     class Program
     {
         static void Main(string[] args)
         {
 
-            FinalClass finalClass = new FinalClass();
-            finalClass.method();
-            myclass myclass = new myclass();
-            myclass.finalmethod();
-              Console.ReadKey();
+          
+
            
         }
     }
