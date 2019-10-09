c750a61135df138e07951f46d1c2816ef9e02535
Qawdaya <Qawdaya@LAPTOP-F1P37JFE>
2019-10-09 16:14:09 +0800

    5.6.4  泛型方法规范

---
+++ 5.6.4  泛型方法规范/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _5._6._4__泛型方法规范
+{
+    public class MethodOverloads
+    {
+        public void Foo<T>(T obj)
+        {
+            Console.WriteLine("Foo<T>(T obj), obj type: {0}", obj.GetType().Name);
+        }
+        public void Foo(int x)
+        {
+            Console.WriteLine("Foo(int x)");
+        }
+        public void Bar<T>(T obj)
+        {
+            Foo(obj);
+        }
+    }
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            var test = new MethodOverloads();
+            test.Foo(33);
+            test.Foo("abc");
+            test.Bar(44);
+            Console.ReadKey();
+
+
+        }
+    }
+}
