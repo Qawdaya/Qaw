2588a4441304a3275823979f8d2f15b0f77e9278
Qawdaya <Qawdaya@LAPTOP-F1P37JFE>
2019-10-09 16:25:10 +0800

    6.2.1  数组的声明 6.2.2  数组的初始化 6.2.3  访问数组元素

---
+++ 6.2  简单数组/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _6._2__简单数组
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myarray = new int[] { 1, 2, 3, 4 };
+            int v1 = myarray[0];
+            int v2 = myarray[1];
+            int v3 = myarray[2];
+            Console.WriteLine("{0} {1} {2}", v1, v2, v3);
+            for(int i=0;i<myarray.Length;i++)
+            {
+                Console.WriteLine(myarray[i]);
+            }
+            foreach(int i in myarray)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
