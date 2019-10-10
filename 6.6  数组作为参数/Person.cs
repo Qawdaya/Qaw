adf91322bc162567745c9e96d04a1526383b12df
Qawdaya <Qawdaya@LAPTOP-F1P37JFE>
2019-10-10 14:18:32 +0800

    6.6.1  数组协变

---
+++ 6.6  数组作为参数/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _6._6__数组作为参数
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public override string ToString()
+        {
+            return String.Format("{0} {1}", FirstName, LastName);
+        }
+    }
+}
---
+++ 6.6  数组作为参数/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _6._6__数组作为参数
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static Person[] GetPerson()
+            {
+            return new Person[]
+            {
+                new Person{FirstName="damon",LastName="Hill"},
+                 new Person{FirstName="Niki",LastName="Lauda"},
+                  new Person{FirstName="Ayrton",LastName="Sonna"},
+                   new Person{FirstName="Graham",LastName="Hill"},
+            };
+            }
+        static void DisplayArray(object[]data)
+        {
+
+        }
+    }
+}
