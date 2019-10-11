9ecf5dcba7ecca207f697122b79fa23d61706349
Qawdaya <Qawdaya@LAPTOP-F1P37JFE>
2019-10-12 01:47:07 +0800

    7.6  用户定义的类型强制转换

---
+++ 7.6  用户定义的类型强制转换/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace _7._6__用户定义的类型强制转换
+{
+    struct Currency
+    {
+        public uint Dollars;
+        public ushort Cents;
+        public Currency(uint dollars,ushort cents)
+        {
+            this.Cents = cents;
+            this.Dollars = dollars;
+        }
+        public override string ToString()
+        {
+            return string.Format("${0}.{1,-2:00}", Dollars, Cents);
+        }
+        public static implicit operator float(Currency value)
+        {
+            return value.Dollars + (value.Cents / 100.0f);
+        }
+        public static explicit operator Currency(float value)
+        {
+            uint dollars = (uint)value;
+            ushort cents = (ushort)((value - dollars) * 100);
+            return new Currency(dollars, cents);
+        }
+    }
+    class Program
+    {
+
+     
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                Currency balance = new Currency(50, 35);
+
+                Console.WriteLine(balance);
+                Console.WriteLine("balance is " + balance);
+                Console.WriteLine("balance is (using ToString()) " + balance.ToString());
+
+                float balance2 = balance;
+
+                Console.WriteLine("After converting to float, = " + balance2);
+
+                balance = (Currency)balance2;
+
+                Console.WriteLine("After converting back to Currency, = " + balance);
+                Console.WriteLine("Now attempt to convert out of range value of " +
+                                  "-$50.50 to a Currency:");
+
+                checked
+                {
+                    balance = (Currency)(-50.50);
+                    Console.WriteLine("Result is " + balance.ToString());
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception occurred: " + e.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
