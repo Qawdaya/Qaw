66d52ac763772258e51efd74cb307380ea9eb9e2
Qawdaya <Qawdaya@LAPTOP-F1P37JFE>
2019-10-12 00:34:47 +0800

    7.4.1  比较引用类型的相等性

---
+++ 7.4  比较对象的相等性/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace _7._4__比较对象的相等性
+{
+    class x
+    {
+
+    }
+    class y
+    {
+
+    }
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            /*ReferenceEquals()方法*/
+            x x = new x();
+            y y = new y();
+            bool B1 = ReferenceEquals(null, null);
+            bool B2 = ReferenceEquals(x, null);
+            bool B3 = ReferenceEquals(x, y);
+            Console.WriteLine(B1);
+            Console.WriteLine(B2);
+            Console.WriteLine(B3);
+            
+        }
+    }
+}
