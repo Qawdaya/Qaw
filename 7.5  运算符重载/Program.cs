cde97ac40d05ec8e37214454fbc8ca8af072a4f7
Qawdaya <Qawdaya@LAPTOP-F1P37JFE>
2019-10-12 00:46:59 +0800

    7.5.1  运算符的工作方式

---
+++ 7.5  运算符重载/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace _7._5__运算符重载
+{
+    
+  
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int myInteger = 3;
+            uint myUnsignedInt = 2;
+            double myDouble = 4.0;
+            long myLong = myInteger + myUnsignedInt;
+            double myOtherDouble = myDouble + myInteger;
+            Console.WriteLine(myLong);
+            Console.WriteLine(myOtherDouble);
+
+            
+        }
+    }
+}
