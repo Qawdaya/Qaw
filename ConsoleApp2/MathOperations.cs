252ec32462926229a6ec624e1a532c55238d4355
Qawdaya <Qawdaya@LAPTOP-F1P37JFE>
2019-10-12 15:56:06 +0800

    8.2.3  简单的委托示例

---
+++ ConsoleApp2/MathOperations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class MathOperations
+{
+       public static double MultiplyByTwo(double value)
+        {
+            return value * 2;
+        }
+        public static double Square(double value)
+        {
+            return value * value;
+        }
+}
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    delegate double DoubleOp(double x);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoubleOp[] operations =
+            {
+                MathOperations.MultiplyByTwo,
+                MathOperations.Square
+        };
+            for(int i=0;i<operations.Length;i++)
+            {
+                Console.WriteLine("Using operations[{0}]:", i);
+                ProcessAndDisplyNumber(operations[i], 2.0);
+                ProcessAndDisplyNumber(operations[i], 7.94);
+                ProcessAndDisplyNumber(operations[i], 1.414);
+
+            }
+
+        }
+         static void ProcessAndDisplyNumber(DoubleOp action,double value)
+        {
+            double result = action(value);
+            Console.WriteLine("Value is {0},result of operation is {1}", value, result);
+        }
+    }
+}
