736516f5ff172613b554f6bec95801ddaec84139
Qawdaya <Qawdaya@LAPTOP-F1P37JFE>
2019-10-13 22:21:43 +0800

    8.2.6  多播委托

---
+++ 8.2.6  多播委托/MathOperations.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _8._2._6__多播委托
+{
+    class MathOperations
+    {
+        public static void MutiplyByTwo(double value)
+        {
+            double result = value * 2;
+            Console.WriteLine("Mutiplying by 2:{0} give {1}", value, result);
+
+        }
+        public static void Square(double value)
+        {
+            double result = value * value;
+            Console.WriteLine("Squaring:{0} give {1}", value, result);
+        }
+    }
+}
---
+++ 8.2.6  多播委托/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _8._2._6__多播委托
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<double> operations = MathOperations.MutiplyByTwo;
+            operations += MathOperations.Square;
+            ProcessAndDisplayNumber(operations, 2.0);
+            ProcessAndDisplayNumber(operations, 7.94);
+            ProcessAndDisplayNumber(operations, 1.414);
+            Console.WriteLine();
+        }
+        static void ProcessAndDisplayNumber(Action<double> action,double value)
+        {
+            Console.WriteLine();
+            Console.WriteLine("ProcessAndDisplayNumber called with value = {0}", value);
+            action(value);
+        }
+    }
+}
