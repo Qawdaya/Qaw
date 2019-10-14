8bae574285f7b00344122f21c1f077ebc8dc0c89
Qawdaya <Qawdaya@LAPTOP-F1P37JFE>
2019-10-14 14:15:23 +0800

    8.2.7  匿名方法

---
+++ 8.2.7  匿名方法/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _8._2._7__匿名方法
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mid = ",middle part";
+            Func<string, string> anonDel = delegate (string param)
+               {
+                   param += mid;
+                   param += "and this was added to the string.";
+                   return param;
+               };
+            Console.WriteLine(anonDel("Start of string"));
+        }
+    }
+}
