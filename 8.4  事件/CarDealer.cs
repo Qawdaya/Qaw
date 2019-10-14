56f095c22cb9555c6b92ee19b0ed2f9b8e38f849
Qawdaya <Qawdaya@LAPTOP-F1P37JFE>
2019-10-14 15:19:09 +0800

    8.4.1  事件发布程序

---
+++ 8.4  事件/CarDealer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _8._4__事件
+{
+   public class CarInfoEventArgs:EventArgs
+    {
+        public CarInfoEventArgs(string car)
+        {
+            this.Car = car;
+        }
+        public string Car { get; set; }
+    }
+    public  class CarDealer
+    {
+        public event EventHandler<CarInfoEventArgs> newCarInfo;
+        public void NewCar(string car)
+        {
+            Console.WriteLine("CarDealer,new car{0}", car);
+            RaiseNewCarInfo(car);
+        }
+        protected virtual void RaiseNewCarInfo(string car)
+        {
+            EventHandler<CarInfoEventArgs> newCarInfo = newCarInfo;
+        if(newCarInfo!=null)
+            {
+                newCarInfo(this, new CarInfoEventArgs(car));
+            }
+
+        }
+    }
+}
---
+++ 8.4  事件/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _8._4__事件
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
